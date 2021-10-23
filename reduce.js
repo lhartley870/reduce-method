5b389fa738df5217161fa10a1d4c57f9e3dd6292
lhartley870 <hartleylc@hotmail.co.uk>
2021-10-23 14:46:30 +0000

    Reduce method examples

--- reduce.js
+++ reduce.js
@@ -6,7 +6,9 @@
 
 
 // Summing an array of numbers:
-
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 const teamMembers = [
   {
@@ -28,10 +30,58 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  }, 
+  {
+    name: 'Michelle',
+    profession: 'Developer',
+    yrsExperience: 12
   }
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} is what we want to achieve
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession; 
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience; 
+  }
+  return acc;
+}, {}); 
+
+console.log(experienceByProfession); 
+
+let professionMembers = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession; 
+  if (!acc[key]) {
+    acc[key] = [curr.name];
+  } else {
+    acc[key].push(curr.name);
+  }
+  return acc;
+}, {})
+
+console.log(professionMembers);
+
+const justDevelopers = teamMembers.filter(member => member.profession === 'Developer');
+console.log(justDevelopers); 
+
+let mostExperiencedDeveloper = justDevelopers.reduce((acc, curr) => {
+  if (curr.yrsExperience > acc.yrsExperience) {
+    acc.name = curr.name; 
+    acc.yrsExperience = curr.yrsExperience;
+  };
+
+  return acc;
+})
+
+console.log(mostExperiencedDeveloper);
+
+let nameMostExperiencedDev = mostExperiencedDeveloper.name;
+console.log(nameMostExperiencedDev);
